71eef3beedab61f50f58465c0d61092450462ac3
val-nvs <153785603+val-nvs@users.noreply.github.com>
2025-10-15 19:38:51 +0100

    etc

--- shared.js
+++ shared.js
@@ -14,12 +14,12 @@ function createHeader() {
     
     <nav aria-label="Menu principal">
       <ul>
-        <li><a href="eventos.html">Eventos</a></li>
-        <li><a href="biblioteca.html">Biblioteca</a></li>
-        <li><a href="cinemateca.html">Cinemateca</a></li>
-        <li><a href="mercado.html">Mercado</a></li>
-        <li><a href="comunidad.html">Comunidad</a></li>
-        <li><a href="contactos.html">Contactos</a></li>
+        <li><a href="/eventos.html">Eventos</a></li>
+        <li><a href="/biblioteca/index.html">Biblioteca</a></li>
+        <li><a href="/cinemateca.html">Cinemateca</a></li>
+        <li><a href="/mercado.html">Mercado</a></li>
+        <li><a href="/comunidad.html">Comunidad</a></li>
+        <li><a href="/contactos.html">Contactos</a></li>
       </ul>
     </nav>
   `;
